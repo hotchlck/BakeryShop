d973d7eb127a07d0ff86a4f1aac6cfeca5905e0b
hotchlck <salma.kurnia@ui.ac.id>
2023-09-26 23:45:45 +0700

    menambahkan fungsi bonus

--- main/urls.py
+++ main/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('add-item', add_item, name='add_item'),
-    path('minus-item', minus_item,name='minus_item'),
-    path('remove-item', remove_item, name='remove_item'),
+    path('add-item/<int:id_item>/', add_item, name='add_item'),
+    path('minus-item/<int:id_item>/', minus_item,name='minus_item'),
+    path('remove-item<int:id_item>/', remove_item, name='remove_item'),
 ]
